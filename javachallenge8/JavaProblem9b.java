64670e9d4042f61da921283e5949184a8578d5ff
priyatharishini <61748229+priyatharishini@users.noreply.github.com>
2023-03-10 23:06:36 +0530

    Add files via upload

---
+++ javachallenge8/JavaProblem9b.java
@@ -0,0 +1,48 @@
+package javachallenge8;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class JavaProblem9b {
+	public static int firstUniqChar(String s)
+	{
+        Set<Character> repeating = new HashSet<Character>(); 
+        Set<Character> nonRepeating = new LinkedHashSet<Character>(); 
+        for (char c : s.toCharArray()) {
+            if (repeating.contains(c)) {
+                continue; 
+            }
+           
+            if (nonRepeating.contains(c)) {
+                nonRepeating.remove(c); 
+                repeating.add(c);
+                System.out.println("Repeating Character");
+                System.out.println(repeating);
+            }
+         
+            else 
+            {
+                nonRepeating.add(c);
+                
+            }
+            
+            System.out.println("Non Repeating Character");
+            System.out.println(nonRepeating);
+        }
+        
+        if (nonRepeating.isEmpty()) {
+            return -1; 
+        } else {
+            return s.indexOf(nonRepeating.iterator().next()); 
+        }
+    }
+	public static void main(String[] args) {
+
+		String s =  "aabb";
+        int result = firstUniqChar(s);
+        System.out.println(result);	
+
+	}
+
+}
